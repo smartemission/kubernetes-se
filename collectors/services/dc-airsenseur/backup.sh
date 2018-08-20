38f867bee2edd7496ca67bcd754dcb94d271c5a4
Just van den Broecke <just@justobjects.nl>
2018-08-20 14:03:08 +0200

    smartemission/smartemission#139 DC InfluxDB backup/restore

--- collectors/services/dc-airsenseur/backup.sh
+++ collectors/services/dc-airsenseur/backup.sh
@@ -8,7 +8,7 @@ CONTAINER_BACKUP_DIR=/backup/${INFLUXDB_DB}
 CONTAINER_DUMP_FILE=/backup/influxdb_${INFLUXDB_DB}_data.tar.gz
 
 # On RUNNING Pod/Container
-kubectl -n collectors exec ${CONTAINER_NAME} -- rm -rf /backup
+kubectl -n collectors exec ${CONTAINER_NAME} -- rm -rf ${CONTAINER_BACKUP_DIR}
 kubectl -n collectors exec ${CONTAINER_NAME} -- mkdir -p ${CONTAINER_BACKUP_DIR}
 kubectl -n collectors exec ${CONTAINER_NAME} -- influxd backup -database ${INFLUXDB_DB} ${CONTAINER_BACKUP_DIR}
 kubectl -n collectors exec ${CONTAINER_NAME} -- tar -cvzf ${CONTAINER_DUMP_FILE} -C /backup ${INFLUXDB_DB}
